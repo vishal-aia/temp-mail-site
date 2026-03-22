8982d6c6a29e85fb14974187479d562af8d40955
vishal-aia <vr7847922@gmail.com>
2026-03-22 10:24:00 +0530

    Add email generation endpoint
    Implement email generation using external API

--- api/generate.js
+++ api/generate.js
@@ -1,1 +1,11 @@
+const fetch = require('node-fetch');
 
+module.exports = async (req, res) => {
+  try {
+    const response = await fetch('https://www.1secmail.com/api/v1/?action=genRandomMailbox&count=1');
+    const data = await response.json();
+    res.status(200).json({ email: data[0] });
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to generate email' });
+  }
+};
